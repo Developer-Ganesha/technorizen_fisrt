f6c7a6a07b4cd59f9eb390990886cb6b1845b68f
Developer-Ganesha <ganeshsavner320@gmail.com>
2025-03-03 16:54:15 +0530

    add host

--- main/main/settings.py
+++ main/main/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-#)&qmwuv+uuz8u9qa&&_#0j$i@7=kcr_+9je728map)iwr202x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS=technorizen-fisrt.onrender.com,127.0.0.1
+
 
 
 # Application definition
