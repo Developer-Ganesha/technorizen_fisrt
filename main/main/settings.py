836827573e2f52df8ea82cee13c3651de8c045bc
Developer-Ganesha <ganeshsavner320@gmail.com>
2025-03-03 17:19:32 +0530

    add os & new host

--- main/main/settings.py
+++ main/main/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-#)&qmwuv+uuz8u9qa&&_#0j$i@7=kcr_+9je728map)iwr202x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['technorizen-fisrt.onrender.com,127.0.0.1']
+import os 
+PORT = int(os.environ.get('PORT',8000))
+
+ALLOWED_HOSTS = ["*"]
+
 
 
 
